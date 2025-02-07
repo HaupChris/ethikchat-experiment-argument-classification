06bbffdfe14960060ccac10fa6cd4d3d1ea81677
chh59ja <christian.hauptmann@uni-wuerzburg.de>
2025-02-07 16:19:25 +0100

    login to wandb

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -33,6 +33,7 @@ def main(exp_config: ExperimentConfig):
     """
     # login to wandb
     api_key = os.getenv("WANDB_API_KEY")
+    print(f"Logging in to wandb with API key: {api_key}")
     wandb.login(key=api_key)
 
     # Initialize wandb
