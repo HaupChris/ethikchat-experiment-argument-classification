6b979afc3f160cb9c5fc4ff5fa16af68b590b566
chh59ja <christian.hauptmann@uni-wuerzburg.de>
2025-02-07 15:28:16 +0100

    add config functionality to create, run and log an experiment

---
+++ src/models/experiment_config.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+
+from src.data.create_corpus_dataset import DatasetSplitType
+
+
+class ExperimentConfig(BaseModel):
+    """
+    Configuration for an experiment run.
+
+    This is used to pass the configuration to the training script and log the configuration.
+    Args:
+        project_root: str (absolute path to the project root)
+        experiment_dir: str (relative path to the experiments directory)
+        experiment_run: str (name of the experiment run)
+        dataset_dir: str (relative path to the dataset directory)
+        dataset_name: str (name of the dataset)
+        dataset_split_type: DatasetSplitType, e.g. DatasetSplitType.Simple
+        model_name: str (name of the model to train)
+        model_name_escaped: str (model name with "/" replaced by "-")
+        model_run_dir: str (absolute path to the model run directory, e.g. /home/user/my_experiments/this_experiment/experiments_outputs/v0/airnicco8-xlm-roberta-de_lr2e-05_bs4)
+        learning_rate: float
+        batch_size: int
+        num_epochs: int
+        loss_function: str (name of the loss function to use, does not change anything but only for logging)
+        run_time: str (timestamp of the start of the experiment run)
+    """
+    project_root: str
+    experiment_dir: str
+    experiment_run: str
+    dataset_dir: str
+    dataset_name: str
+    dataset_split_type: DatasetSplitType
+    model_name: str
+    model_name_escaped: str
+    model_run_dir: str
+    learning_rate: float
+    batch_size: int
+    num_epochs: int
+    loss_function: str
+    run_time: str
+
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+from datetime import datetime
+
 import wandb
 
 from datasets import load_from_disk
@@ -14,36 +17,35 @@ from sentence_transformers.training_args import BatchSamplers
 from src.data.create_corpus_dataset import DatasetSplitType
 from src.data.dataset_splits import create_splits_from_corpus_dataset
 from src.features.build_features import create_dataset_for_multiple_negatives_ranking_loss
+from src.models.experiment_config import ExperimentConfig
 
 IS_TEST_RUN = True
 
 
-def main(model_name, dataset_name, output_dir, learning_rate):
+def main(exp_config: ExperimentConfig):
+    """
+    Train a sentence transformer model with the given parameters and log the results
+    to wandb.
+
+    Args:
+        exp_config: ExperimentConfig
+    """
     # Initialize wandb
     wandb.init(
-        project="sentence-transformers-training",
-        name=output_dir,  # Set a meaningful name for this run
-        config={
-            "model_name": model_name,
-            "dataset_name": dataset_name,
-            "learning_rate": learning_rate,
-            "batch_size": 4,
-            "epochs": 1,
-        }
+        project="argument-classification",
+        name=exp_config.model_run_dir,
+        config=exp_config.model_dump(),
     )
 
     # Load model
-    model = SentenceTransformer(model_name)
+    model = SentenceTransformer(exp_config.model_name)
 
     # Define loss
     loss = MultipleNegativesRankingLoss(model=model)
 
     # Load dataset
-    dataset = load_from_disk(f"../../data/processed/{dataset_name}")
-    splitted_dataset = create_splits_from_corpus_dataset(dataset, DatasetSplitType.Simple)
-
-
-
+    dataset = load_from_disk(f"{exp_config.project_root}/{exp_config.dataset_dir}/{exp_config.dataset_name}")
+    splitted_dataset = create_splits_from_corpus_dataset(dataset, exp_config.dataset_split_type)
 
     if IS_TEST_RUN:
         train_pos = create_dataset_for_multiple_negatives_ranking_loss(splitted_dataset["train"], 1)
@@ -65,19 +67,19 @@ def main(model_name, dataset_name, output_dir, learning_rate):
         corpus=eval_corpus,
         queries=eval_queries,
         relevant_docs=eval_relevant_passages,
-        name=f"{output_dir}-eval",
         show_progress_bar=True,
         write_csv=True,
     )
 
     ir_evaluator_eval(model)
 
-    args = SentenceTransformerTrainingArguments(
-        output_dir=f"../../models/{output_dir}",
+
+    train_args = SentenceTransformerTrainingArguments(
+        output_dir=exp_config.model_run_dir,
         num_train_epochs=1,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=4,
-        learning_rate=learning_rate,
+        per_device_train_batch_size=int(exp_config.batch_size),
+        per_device_eval_batch_size=int(exp_config.batch_size),
+        learning_rate=exp_config.learning_rate,
         warmup_ratio=0.1,
         fp16=False,
         bf16=False,
@@ -88,13 +90,12 @@ def main(model_name, dataset_name, output_dir, learning_rate):
         save_steps=100,
         save_total_limit=2,
         logging_steps=29,
-        run_name=output_dir,  # Sync run name with wandb
+        run_name=f"{exp_config.model_name_escaped}_lr{exp_config.learning_rate}_bs{exp_config.batch_size}_{exp_config.run_time}",  # Sync run name with wandb
         load_best_model_at_end=True
     )
-
     trainer = SentenceTransformerTrainer(
         model=model,
-        args=args,
+        args=train_args,
         train_dataset=train_pos,
         eval_dataset=eval_pos,
         loss=loss,
@@ -104,13 +105,16 @@ def main(model_name, dataset_name, output_dir, learning_rate):
     trainer.train()
 
     # Run final evaluation
-    ir_evaluator_eval(model)
+    final_eval_results = ir_evaluator_eval(model)
+    wandb.log(final_eval_results)
 
     # Save model to wandb as an artifact
-    model_dir = f"../../models/{output_dir}"
-    artifact = wandb.Artifact(name=f"{output_dir}-model", type="model")
-    artifact.add_dir(model_dir)
+
+    artifact = wandb.Artifact(name=f"{exp_config.model_run_dir}", type="model")
+    artifact.add_dir(exp_config.model_run_dir)
     wandb.log_artifact(artifact)
+
+    # save the slurm output to wandb
 
     # Finish wandb run
     wandb.finish()
@@ -122,19 +126,73 @@ if __name__ == "__main__":
     parser.add_argument('--dataset_name', type=str, help='Path to the dataset.')
     parser.add_argument('--output_dir', type=str, help='Path to the output directory.')
     parser.add_argument('--learning_rate', type=float, help='Learning rate.')
+    parser.add_argument('--batch_size', type=str, help='Batch size.')
     args = parser.parse_args()
+
+    expirment_timestamp_start = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     if args.model_name is None:
         print("Starting training in testing mode...")
 
-        model_name = "airnicco8/xlm-roberta-de"
-        dataset_name = "corpus_dataset_experiment_v0"
-        learning_rate = 2e-05
 
-        model_name_escaped = model_name.replace("/", "-")
-        output_dir = f"output_test_crf/{dataset_name}/{model_name_escaped}/lr{learning_rate}"
+        args_project_root = "/home/christian/PycharmProjects/ethikchat-experiment-argument-classification"
+        args_experiment_run = "v0"
+        args_experiment_dir = "experiments_outputs"
+        args_dataset_dir = "data/processed"
+        args_dataset_name = "corpus_dataset_experiment_v0"
+        args_model_name = "airnicco8/xlm-roberta-de"
+        args_model_name_escaped = args_model_name.replace("/", "-")
+        args_learning_rate = 2e-05
+        args_batch_size = 4
+        args_model_run_dir = os.path.join(args_project_root, args_experiment_dir, args_experiment_run, f"{args_model_name_escaped}_lr{args_learning_rate}_bs{args_batch_size}")
 
-        print(f"Running test training for model {model_name}")
-        main(model_name, dataset_name, output_dir, learning_rate)
+
+        experiment_config = {
+            "project_root": args_project_root,
+            "experiment_dir": args_experiment_dir,
+            "experiment_run": args_experiment_run,
+            "dataset_dir": args_dataset_dir,
+            "dataset_name": args_dataset_name,
+            "model_name": args_model_name,
+            "model_name_escaped": args_model_name_escaped,
+            "learning_rate": args_learning_rate,
+            "batch_size": args_batch_size,
+            "model_run_dir": args_model_run_dir,
+            "dataset_split_type": DatasetSplitType.Simple,
+            "num_epochs": 2,
+            "loss_function": "MultipleNegativesRankingLoss",
+            "run_time": expirment_timestamp_start
+        }
+
+        experiment_config = ExperimentConfig(**experiment_config)
+
+
+        if not os.path.exists(args_model_run_dir):
+            os.makedirs(args_model_run_dir)
+
+        print(f"Running test training for model {args_model_name}")
+        main(experiment_config)
     else:
-        main(args.model_name, args.dataset_name, args.output_dir, args.learning_rate)
+        args_batch_size = int(args.batch_size)
+        args_learning_rate = float(args.learning_rate)
+
+        experiment_config = {
+            "project_root": args.project_root,
+            "experiment_dir": args.experiment_dir,
+            "experiment_run": args.experiment_run,
+            "dataset_dir": args.dataset_dir,
+            "dataset_name": args.dataset_name,
+            "model_name": args.model_name,
+            "model_name_escaped": args.model_name_escaped,
+            "learning_rate": args_learning_rate,
+            "batch_size": args_batch_size,
+            "model_run_dir": args.model_run_dir,
+            "dataset_split_type": DatasetSplitType.from_str(args.dataset_split_type),
+            "num_epochs": args.num_epochs,
+            "loss_function": args.loss_function,
+            "run_time": expirment_timestamp_start
+        }
+
+        experiment_config = ExperimentConfig(**experiment_config)
+
+        main(experiment_config)
